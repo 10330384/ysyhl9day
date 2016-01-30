13fcb89b097121a92f16de45c787df1580d95079
10330384 <214688716@qq.com>
2016-01-30 09:56:49 +0800

    XXXX

---
+++ Tools/Scripts/cyutchecker/REfuckmap.py
@@ -0,0 +1,49 @@
+#V201601300944RE
+127.0.0.1 localhost
+::1 localhost
+#Google Map START
+#1
+#64.15.122.59    android.clients.google.com
+#2
+64.15.122.59    clients1.google.com
+64.15.122.59    clients2.google.com
+64.15.122.59    clients3.google.com
+64.15.122.59    clients4.google.com
+64.15.122.59    clients5.google.com
+64.15.122.59    clients6.google.com
+64.15.122.59    clients7.google.com
+#3
+#64.15.122.59    clients1.googleusercontent.com
+#64.15.122.59    clients2.googleusercontent.com
+#64.15.122.59    clients3.googleusercontent.com
+#64.15.122.59    clients4.googleusercontent.com
+#64.15.122.59    clients5.googleusercontent.com
+#64.15.122.59    clients6.googleusercontent.com
+#64.15.122.59    clients7.googleusercontent.com
+#64.15.122.59    lh0.googleusercontent.com
+#64.15.122.59    lh1.googleusercontent.com
+#64.15.122.59    lh2.googleusercontent.com
+#64.15.122.59    lh3.googleusercontent.com
+#64.15.122.59    lh4.googleusercontent.com
+#64.15.122.59    lh5.googleusercontent.com
+#64.15.122.59    lh6.googleusercontent.com
+#4
+64.15.122.59    gm0.ggpht.com
+64.15.122.59    gm5.ggpht.com
+64.15.122.59    gm6.ggpht.com
+64.15.122.59    geo0.ggpht.com
+64.15.122.59    geo1.ggpht.com
+64.15.122.59    geo2.ggpht.com
+64.15.122.59    geo3.ggpht.com
+64.15.122.59    geo4.ggpht.com
+64.15.122.59    geo5.ggpht.com
+64.15.122.59    geo6.ggpht.com
+#5
+64.15.122.59    lh1.ggpht.com
+64.15.122.59    lh2.ggpht.com
+64.15.122.59    lh3.ggpht.com
+64.15.122.59    lh4.ggpht.com
+64.15.122.59    lh5.ggpht.com
+64.15.122.59    lh6.ggpht.com
+64.15.122.59    lh0.ggpht.com
+#Google Map END
--- Tools/versioncheck/version.py
+++ Tools/versioncheck/version.py
@@ -1,4 +1,21 @@
-V201601282212AD
-V201601300035RE
-V201601300035AR
+"""This file (which is sourced, not imported) checks the version of the
+"versioncheck" package. It is also an example of how to format your own
+_checkversion.py file""" """ V201601282212AD     """
+ 
+import pyversioncheck
+
+_PACKAGE="versioncheck"
+_VERSION=" V201601300944AR "
+_URL="http://www.cwi.nl/~jack/versioncheck/curversion.txt"
+
+try:
+    _myverbose=  V201601300944RE
+except NameError:
+    _myverbose=1
+
+pyversioncheck.versioncheck(_PACKAGE, _URL, _VERSION, verbose=_myverbose)
+
+
+
+
 52061452
