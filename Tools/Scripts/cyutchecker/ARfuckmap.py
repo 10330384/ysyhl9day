76527edbdd7306b32ec031218c3ff496047ed2bf
10330384 <214688716@qq.com>
2016-01-30 10:06:11 +0800

    XXXX

--- Tools/Scripts/cyutchecker/ARfuckmap.py
+++ Tools/Scripts/cyutchecker/ARfuckmap.py
@@ -3,49 +3,49 @@
 ::1 localhost
 #Google Map START
 #1
-#64.15.122.59    android.clients.google.com
+#74.125.232.207    android.clients.google.com
 #2
-64.15.122.59    clients1.google.com
-64.15.122.59    clients2.google.com
-64.15.122.59    clients3.google.com
-64.15.122.59    clients4.google.com
-64.15.122.59    clients5.google.com
-64.15.122.59    clients6.google.com
-64.15.122.59    clients7.google.com
+74.125.232.207    clients1.google.com
+74.125.232.207    clients2.google.com
+74.125.232.207    clients3.google.com
+74.125.232.207    clients4.google.com
+74.125.232.207    clients5.google.com
+74.125.232.207    clients6.google.com
+74.125.232.207    clients7.google.com
 #3
-#64.15.122.59    clients1.googleusercontent.com
-#64.15.122.59    clients2.googleusercontent.com
-#64.15.122.59    clients3.googleusercontent.com
-#64.15.122.59    clients4.googleusercontent.com
-#64.15.122.59    clients5.googleusercontent.com
-#64.15.122.59    clients6.googleusercontent.com
-#64.15.122.59    clients7.googleusercontent.com
-#64.15.122.59    lh0.googleusercontent.com
-#64.15.122.59    lh1.googleusercontent.com
-#64.15.122.59    lh2.googleusercontent.com
-#64.15.122.59    lh3.googleusercontent.com
-#64.15.122.59    lh4.googleusercontent.com
-#64.15.122.59    lh5.googleusercontent.com
-#64.15.122.59    lh6.googleusercontent.com
+#74.125.232.207    clients1.googleusercontent.com
+#74.125.232.207    clients2.googleusercontent.com
+#74.125.232.207    clients3.googleusercontent.com
+#74.125.232.207    clients4.googleusercontent.com
+#74.125.232.207    clients5.googleusercontent.com
+#74.125.232.207    clients6.googleusercontent.com
+#74.125.232.207    clients7.googleusercontent.com
+#74.125.232.207    lh0.googleusercontent.com
+#74.125.232.207    lh1.googleusercontent.com
+#74.125.232.207    lh2.googleusercontent.com
+#74.125.232.207    lh3.googleusercontent.com
+#74.125.232.207    lh4.googleusercontent.com
+#74.125.232.207    lh5.googleusercontent.com
+#74.125.232.207    lh6.googleusercontent.com
 #4
-64.15.122.59    gm0.ggpht.com
-64.15.122.59    gm5.ggpht.com
-64.15.122.59    gm6.ggpht.com
-64.15.122.59    geo0.ggpht.com
-64.15.122.59    geo1.ggpht.com
-64.15.122.59    geo2.ggpht.com
-64.15.122.59    geo3.ggpht.com
-64.15.122.59    geo4.ggpht.com
-64.15.122.59    geo5.ggpht.com
-64.15.122.59    geo6.ggpht.com
+74.125.232.207    gm0.ggpht.com
+74.125.232.207    gm5.ggpht.com
+74.125.232.207    gm6.ggpht.com
+74.125.232.207    geo0.ggpht.com
+74.125.232.207    geo1.ggpht.com
+74.125.232.207    geo2.ggpht.com
+74.125.232.207    geo3.ggpht.com
+74.125.232.207    geo4.ggpht.com
+74.125.232.207    geo5.ggpht.com
+74.125.232.207    geo6.ggpht.com
 #5
-64.15.122.59    lh1.ggpht.com
-64.15.122.59    lh2.ggpht.com
-64.15.122.59    lh3.ggpht.com
-64.15.122.59    lh4.ggpht.com
-64.15.122.59    lh5.ggpht.com
-64.15.122.59    lh6.ggpht.com
-64.15.122.59    lh0.ggpht.com
+74.125.232.207    lh1.ggpht.com
+74.125.232.207    lh2.ggpht.com
+74.125.232.207    lh3.ggpht.com
+74.125.232.207    lh4.ggpht.com
+74.125.232.207    lh5.ggpht.com
+74.125.232.207    lh6.ggpht.com
+74.125.232.207    lh0.ggpht.com
 #Google Map END
 #NO AD START
 127.0.0.1 ad.doubleclick.net.80911.9682.302br.net
